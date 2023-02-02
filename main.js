97a5e3ec5db506fb10661709fec0b079ffb9010b
whitehatsoham <100528214+whitehatsoham@users.noreply.github.com>
2023-02-02 16:38:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+song_1 = "";
+song_2 = "";
+
+song1_status="";
+song2_status="";
+
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload(){
+    song_1=loadSound("music.mp3");
+    song_2=loadSound("music2.mp2");
+}
+
+function setup(){
+canvas=createCanvas(600, 500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide;
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet IS Initialized')
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +leftWristX +" leftWristY = "+ leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightWristX +" rightWristY = "+ rightWristY)
+
+
+
+    }
+}
